20e6fc7458e983da24be493f0a08b872c8dc4960
Satya2329 <satyapriyap67@gmail.com>
2026-08-04 23:59:22 +0530

    Infinity Loop

---
+++ LoopControl/Condn.java
@@ -0,0 +1,10 @@
+package LoopControl;
+
+public class Condn {
+    public static void main(String[] args) {
+        int n=0;
+        while(n>0){
+            System.out.println("Infinity");
+        }
+    }
+}
