1e182d0d25b2e464c0c53f4358296828e51e51bc
Satya2329 <satyapriyap67@gmail.com>
2026-08-06 19:06:27 +0530

    Complete of while loop

--- LoopControl/LastNum.java
+++ LoopControl/LastNum.java
@@ -1,16 +1,19 @@
 package LoopControl;
 import java.util.Scanner;
+
 public class LastNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of num:");
         int num = sc.nextInt();
 
-        while(num > 0){
-            int digit = num%10;
-            System.out.println(digit);
-            num = num/10;
+        num = Math.abs(num);
+        while (true) {
+            int digit = num % 10;
+            System.out.println("The last digit is: " + digit);
+            break; // Stops the loop after printing the first (last) digit
         }
+
         sc.close();
     }
 }
