8953e4d175e00323070cfed088bf53771e686248
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-04 17:55:28 +0530

    Largest Element in an Array

---
+++ BasicCode/LargestElement.java
@@ -0,0 +1,15 @@
+package BasicCode;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70};
+        int max = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            } 
+        }
+        System.out.println("Largest element  is" + max);
+
+    }
+}
