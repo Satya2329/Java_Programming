69df90b10cb651debb7ca8f812ce7ee0ab75976a
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-05 23:47:07 +0530

    Reverse of an array

---
+++ BasicCode/ReverseOfArray.java
@@ -0,0 +1,11 @@
+package BasicCode;
+
+public class ReverseOfArray {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40};
+
+        for(int i=arr.length-1; i>=0; i--){
+        System.out.println(arr[i]);
+    }
+}
+}
