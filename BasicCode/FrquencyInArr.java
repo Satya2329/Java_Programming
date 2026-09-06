b37bd3554348ad29a2f81aba0764de2ab09b2710
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-06 23:26:23 +0530

    Frequency In an array

---
+++ BasicCode/FrquencyInArr.java
@@ -0,0 +1,26 @@
+package BasicCode;
+
+public class FrquencyInArr {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 10, 30, 40, 40, 10};
+        boolean[] visited = new boolean[arr.length];
+
+        System.out.println("Element | Frequency");
+        System.out.println("-------------------");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+
+            System.out.println(arr[i] + "      | " + count);
+        }
+    }
+}
