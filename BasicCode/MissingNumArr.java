fa6a9810301384470dc87072e738c0f332f5f73a
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-06 23:19:15 +0530

    Find missing elements in an array

---
+++ BasicCode/MissingNumArr.java
@@ -0,0 +1,18 @@
+package BasicCode;
+
+public class MissingNumArr {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 6}; 
+        int n = arr.length + 1;
+        int expectedSum = n * (n + 1) / 2;
+        
+        int actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
+        }
+        
+        int missingNumber = expectedSum - actualSum;
+        
+        System.out.println("Missing number is: " + missingNumber);
+    }
+}
