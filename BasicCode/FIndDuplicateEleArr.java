72b2ffdd344c08ce302118666ec02acf283a10d9
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-06 23:10:26 +0530

    Find the Duplicate element in an array

---
+++ BasicCode/FIndDuplicateEleArr.java
@@ -0,0 +1,18 @@
+package BasicCode;
+
+public class FIndDuplicateEleArr {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 10, 30, 40, 40};
+
+        System.out.println("Duplicate elements in the array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
