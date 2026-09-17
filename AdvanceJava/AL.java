0beb7a5830918d5c49f89ab57777658328ee3447
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-09-17 12:13:55 +0530

    ArrayList Example

---
+++ AdvanceJava/AL.java
@@ -0,0 +1,13 @@
+package AdvanceJava;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AL {
+    public static void main(String[] args) {
+        List<Integer> al = Arrays.asList(10,15,25,16);
+        al.forEach(System.out::println);
+    }
+}
